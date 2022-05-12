aaa4d19876b3ba68c869643ddf9475bd48e66ed5
ritoraj917 <ritorajarb@gmail.com>
2022-05-12 18:30:47 +0530

    my first commit

---
+++ server/app.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const auth = require('./router/auth')
+const cookieParser = require("cookie-parser");
+
+dotenv.config({ path: './config.env'});
+require('./db/conn');
+//const User = require('./model/teacherSchema');
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+
+
+// middleware
+
+// const middleware = (req, res, next) => {
+//     console.log('hello middleware');
+// }
+
+//middleware();
+
+app.get('/', (req, res) => {
+    res.send('Hello World from the server');
+});
+
+app.use("/auth", auth);
+// app.get('/about', (req, res) => {
+//     res.send('Hello About World from the server');
+// });
+
+// app.get('/about', (req, res) => {
+//     res.send('Hello Contact World from the server');
+// });
+
+app.get('/signin', (req, res) => {
+    res.send('Hello Signin Contact World from the server');
+});
+
+app.get('/signup', (req, res) => {
+    res.send('Hello Signup Contact World from the server');
+});
+
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"));
+}
+
+app.listen(PORT, () => {
+    console.log(`server running at p_no ${PORT}`);
+})
+
